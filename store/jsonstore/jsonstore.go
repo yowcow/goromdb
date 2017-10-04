f8e2b5b54052111c0456c5890822bb7c0a00e565
yowcow <yowcow@cpan.org>
2017-10-04 12:32:38 +0900

    improve store

--- store/jsonstore/jsonstore.go
+++ store/jsonstore/jsonstore.go
@@ -24,6 +24,7 @@ type JSONStore struct {
 func New(file string) (store.Store, error) {
 	in := make(chan *string)
 	out := make(chan *string)
+	boot := make(chan bool)
 	quit := make(chan bool)
 	wg := new(sync.WaitGroup)
 	logger := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
@@ -31,7 +32,9 @@ func New(file string) (store.Store, error) {
 	s := &JSONStore{in, out, quit, wg, logger}
 
 	wg.Add(1)
-	go s.startDataNode(file)
+	go s.startDataNode(file, boot)
+	<-boot // await store to boot
+	s.logger.Print("-> store started!")
 
 	return s, nil
 }
@@ -48,71 +51,114 @@ func (s JSONStore) Get(key string) (string, error) {
 func (s JSONStore) Shutdown() error {
 	s.quit <- true
 	s.wg.Wait()
-	s.logger.Print("-> store shutdown completed!")
+
+	close(s.quit)
+	close(s.in)
+	close(s.out)
+	s.logger.Print("-> store finished!")
+
 	return nil
 }
 
-func (s JSONStore) startDataNode(file string) {
+func (s JSONStore) startDataNode(file string, boot chan bool) {
 	defer s.wg.Done()
 
 	var data Data
-	update := make(chan Data)
+	data, err := LoadJSON(file)
 
-	go s.loadJSON(file, update)
-	data = <-update
+	if err != nil {
+		s.logger.Println("-> store initial data load failed: ", err)
+	}
 
-	d := 5 * time.Second
-	t := time.NewTimer(d)
-	fi, _ := os.Stat(file)
-	mt := fi.ModTime()
-	reloading := false
+	watcherupdate := make(chan Data)
+	watcherboot := make(chan bool)
+	watcherquit := make(chan bool)
+	watcherwg := new(sync.WaitGroup)
+
+	watcherwg.Add(1)
+	go s.startWatcher(file, watcherupdate, watcherboot, watcherquit, watcherwg)
+
+	<-watcherboot // await watcher to boot
+	close(boot)
+	s.logger.Print("-> datanode started!")
 
 	for {
 		select {
-		case d := <-update:
+		case d := <-watcherupdate:
 			data = d
-			reloading = false
-			s.logger.Print("-> store loaded with new data!")
-		case <-t.C:
-			fi, _ := os.Stat(file)
-			if fi.ModTime() != mt && !reloading {
-				mt = fi.ModTime()
-				reloading = true
-				s.logger.Print("-> store going to be reloaded!")
-				go s.loadJSON(file, update)
-			}
-			t.Reset(d)
-		case key := <-s.in:
-			if v, ok := data[*key]; ok {
+			s.logger.Print("-> datanode loaded with new data!")
+		case k := <-s.in:
+			if v, ok := data[*k]; ok {
 				s.out <- &v
 			} else {
 				s.out <- nil
 			}
 		case <-s.quit:
-			s.logger.Print("-> store shutting down!")
-			if !t.Stop() {
-				<-t.C
-			}
+			watcherquit <- true
+			watcherwg.Wait()
+
+			close(watcherupdate)
+			close(watcherquit)
+
+			s.logger.Print("-> datanode finished!")
 			return
 		}
 	}
 }
 
-func (s JSONStore) loadJSON(file string, out chan Data) {
+func (s JSONStore) startWatcher(file string, out chan Data, boot chan bool, quit chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var lastModified time.Time
+
+	if fi, err := os.Stat(file); err == nil {
+		lastModified = fi.ModTime()
+	}
+
+	d := 5 * time.Second
+	t := time.NewTimer(d)
+
+	close(boot)
+	s.logger.Print("-> watcher started!")
+
+	for {
+		select {
+		case <-t.C:
+			if fi, err := os.Stat(file); err == nil {
+				if fi.ModTime() != lastModified {
+					lastModified = fi.ModTime()
+					if data, err := LoadJSON(file); err == nil {
+						out <- data
+					} else {
+						s.logger.Print("-> watcher failed reading data from file: ", err)
+					}
+				}
+			} else {
+				s.logger.Print("-> watcher file check failed: ", err)
+			}
+			t.Reset(d)
+		case <-quit:
+			if !t.Stop() {
+				<-t.C
+			}
+			s.logger.Print("-> watcher finished!")
+			return
+		}
+	}
+}
+
+func LoadJSON(file string) (Data, error) {
 	var data Data
-	defer func() {
-		out <- data
-	}()
 
 	b, err := ioutil.ReadFile(file)
 	if err != nil {
-		s.logger.Print(err)
-		return
+		return data, err
 	}
 
 	err = json.Unmarshal(b, &data)
 	if err != nil {
-		s.logger.Print(err)
-		return
+		return data, err
 	}
+
+	return data, nil
 }
