c2edb46eb69ad23231386a304744c0c2b39d4825
yowcow <yowcow@cpan.org>
2017-10-13 10:38:05 +0900

    select data store at boot

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/yowcow/go-romdb/protocol/memcachedprotocol"
 	"github.com/yowcow/go-romdb/server"
+	"github.com/yowcow/go-romdb/store"
+	"github.com/yowcow/go-romdb/store/bdbstore"
 	"github.com/yowcow/go-romdb/store/jsonstore"
 )
 
 func main() {
 	var addr string
+	var storeBackend string
 	var file string
+
 	flag.StringVar(&addr, "addr", ":11211", "Address to bind to")
-	flag.StringVar(&file, "json", "./data/sample-data.json", "JSON data file")
+	flag.StringVar(&storeBackend, "store", "bdb", "Store type: json, bdb")
+	flag.StringVar(&file, "file", "./data/sample-bdb.db", "Data file")
 	flag.Parse()
 
 	proto, err := memcachedprotocol.New()
@@ -21,7 +27,7 @@ func main() {
 		panic(err)
 	}
 
-	store, err := jsonstore.New(file)
+	store, err := createStore(storeBackend, file)
 
 	if err != nil {
 		panic(err)
@@ -29,4 +35,15 @@ func main() {
 
 	s := server.New("tcp", addr, proto, store)
 	s.Start()
+}
+
+func createStore(storeBackend, file string) (store.Store, error) {
+	switch storeBackend {
+	case "bdb":
+		return bdbstore.New(file)
+	case "json":
+		return jsonstore.New(file)
+	default:
+		return nil, fmt.Errorf("don't know how to handle store '%s'", storeBackend)
+	}
 }
