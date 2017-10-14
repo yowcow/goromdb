63e7d2034cbec434557b877b045731091204c430
yowcow <yowcow@cpan.org>
2017-10-14 20:43:09 +0900

    separate watcher

--- store/store.go
+++ store/store.go
@@ -2,11 +2,50 @@ package store
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
 )
 
 type Store interface {
 	Get(string) (string, error)
 	Shutdown() error
+}
+
+func NewWatcher(d time.Duration, file string, logger *log.Logger, update chan<- bool, quit <-chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var lastModified time.Time
+
+	if fi, err := os.Stat(file); err == nil {
+		lastModified = fi.ModTime()
+	}
+
+	t := time.NewTimer(d)
+
+	logger.Print("-> watcher started!")
+
+	for {
+		select {
+		case <-t.C:
+			if fi, err := os.Stat(file); err == nil {
+				if fi.ModTime() != lastModified {
+					lastModified = fi.ModTime()
+					update <- true
+				}
+			} else {
+				logger.Print("-> watcher file check failed: ", err)
+			}
+			t.Reset(d)
+		case <-quit:
+			if !t.Stop() {
+				<-t.C
+			}
+			logger.Print("-> watcher finished!")
+			return
+		}
+	}
 }
 
 func KeyNotFoundError(key string) error {
