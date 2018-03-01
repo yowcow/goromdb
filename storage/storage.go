21766b00404b44f802f6722794b2789050a95585
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2018-03-01 13:39:28 +0900

    add error type

--- storage/storage.go
+++ storage/storage.go
@@ -7,6 +7,16 @@ import (
 // IterationFunc defines an interface to a callback function for LoadAndIterate()
 type IterationFunc func([]byte, []byte) error
 
+// ErrorKeyNotFound key-not-found error type
+type ErrorKeyNotFound struct {
+	error
+}
+
+// ErrorInternal internal error
+type ErrorInternal struct {
+	error
+}
+
 // Storage defines an interface to a storage
 type Storage interface {
 	Get([]byte) ([]byte, error)
@@ -16,5 +26,26 @@ type Storage interface {
 
 // KeyNotFoundError returns an error for key-not-found
 func KeyNotFoundError(key []byte) error {
-	return fmt.Errorf("key not found error: %s", string(key))
+	return &ErrorKeyNotFound{
+		fmt.Errorf("key not found error: %s", string(key)),
+	}
+}
+
+// InternalError something go wrong internal
+func InternalError(s string) error {
+	return &ErrorInternal{
+		fmt.Errorf(s),
+	}
+}
+
+// IsErrorKeyNotFound returns if it is an ErrorKeyNotFound
+func IsErrorKeyNotFound(err error) bool {
+	switch err.(type) {
+	case ErrorKeyNotFound:
+		return true
+	case *ErrorKeyNotFound:
+		return true
+	default:
+		return false
+	}
 }
