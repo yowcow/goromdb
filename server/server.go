a3e1797dc489acecd4ded0aacde12ffd48a89dba
yowcow <yowcow@cpan.org>
2018-05-28 14:00:50 +0900

    server to only manage tcp/unix socket

--- server/server.go
+++ server/server.go
@@ -5,27 +5,24 @@ import (
 	"io"
 	"log"
 	"net"
-
-	"github.com/yowcow/goromdb/handler"
-	"github.com/yowcow/goromdb/protocol"
 )
+
+type OnReadCallbackFunc func(net.Conn, []byte, *log.Logger)
 
 // Server represents a server
 type Server struct {
-	network  string
-	addr     string
-	protocol protocol.Protocol
-	handler  handler.Handler
-	logger   *log.Logger
+	network string
+	addr    string
+	logger  *log.Logger
 }
 
 // New creates a new server
-func New(network, addr string, p protocol.Protocol, h handler.Handler, logger *log.Logger) *Server {
-	return &Server{network, addr, p, h, logger}
+func New(network, addr string, logger *log.Logger) *Server {
+	return &Server{network, addr, logger}
 }
 
 // Start starts a server and spawns a goroutine when a new connection is accepted
-func (s Server) Start() error {
+func (s Server) Start(callback OnReadCallbackFunc) error {
 	ln, err := net.Listen(s.network, s.addr)
 	if err != nil {
 		return err
@@ -35,13 +32,13 @@ func (s Server) Start() error {
 		if err != nil {
 			s.logger.Printf("server failed accepting a conn: %s", err.Error())
 		} else {
-			go s.HandleConn(conn)
+			go s.HandleConn(conn, callback)
 		}
 	}
 }
 
 // HandleConn handles a net.Conn
-func (s Server) HandleConn(conn net.Conn) {
+func (s Server) HandleConn(conn net.Conn, callback OnReadCallbackFunc) {
 	defer conn.Close()
 	r := bufio.NewReader(conn)
 	for {
@@ -53,15 +50,7 @@ func (s Server) HandleConn(conn net.Conn) {
 			s.logger.Printf("server failed reading a line: %s", err)
 			return
 		}
-		if keys, err := s.protocol.Parse(line); err != nil {
-			s.logger.Printf("server failed parsing a line: %s", err)
-		} else {
-			for _, k := range keys {
-				if v, _ := s.handler.Get(k); v != nil {
-					s.protocol.Reply(conn, k, v)
-				}
-			}
-		}
-		s.protocol.Finish(conn)
+
+		callback(conn, line, s.logger)
 	}
 }
